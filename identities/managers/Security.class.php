9de7c1d2d06f11308f71055dd333177b68e6b68c
Monica Chase <chase.mona@gmail.com>
2011-03-31 08:28:59 -0700

    Added action determineClearance

--- identities/managers/Security.class.php
+++ identities/managers/Security.class.php
@@ -1,9 +1,14 @@
 <?php
 
-// Monica Chase (chase.mona@gmail.com) | 30 March 2011
+// CREATED BY: Monica Chase (chase.mona@gmail.com) | 30 March 2011
+// Revision 1 (31 March 2011) | Monica Chase (chase.mona@gmail.com)
 
 class SecurityManager
 	{
+        protected function determineClearance()     // DEFAULT: Hide from ALL
+        {
+            throw new RuntimeException("You are not authorized.");
+        }
 	}
 
 ?>
