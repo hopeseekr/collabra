7b852c1fc27daef30c3f94dda49af1af0e86d2cd
Monica Chase <chase.mona@gmail.com>
2011-04-01 11:36:01 -0700

    added action ensureValidURL

--- identities/managers/Security.class.php
+++ identities/managers/Security.class.php
@@ -26,6 +26,14 @@ class SecurityManager
 		}
 	}
 
+    protected function ensureValidURL($url)
+    {
+        if (filter_var($url, FILTER_VALIDATE_URL) == false)
+        {
+            throw new RuntimeException("Invalid URL");
+        }
+    }
+    
 	public function guardEntrance(User $user, $url)
 	{
 		$this->ensureHasClearance($user, $url);
