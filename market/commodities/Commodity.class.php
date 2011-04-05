921d40790aa3710056b9bcf53da0aa178eeb8d99
Monica Chase <chase.mona@gmail.com>
2011-04-05 16:24:49 -0700

    added CommodityBasket.class.php added Theodore's copyright to all php files

--- market/commodities/Commodity.class.php
+++ market/commodities/Commodity.class.php
@@ -1,6 +1,7 @@
 <?php
 
 // Monica Chase (chase.mona@gmail.com) | 4 April 2011
+// Copyright (c) 2011 Theodore R. Smith <theodore@phpexperts.pro>
 
 class Commodity
 {
---
+++ market/commodities/CommodityBasket.class.php
@@ -0,0 +1,31 @@
+<?php
+
+// Monica Chase (chase.mona@gmail.com) | 5 April 2011
+// Copyright (c) 2011 Theodore R. Smith <theodore@phpexperts.pro>
+
+class CommodityBasket
+{
+	private $commodities;
+	
+	public function add($commodity)
+	{
+	}
+	
+	public function take($commodity)
+	{
+	}
+	
+	public function fetchQuantity($commodityName)
+	{
+	}
+	
+	public function fetchValuation($commodityName)
+	{
+	}
+	
+	public function fetchSummary($commodityName)
+	{
+	// results of both fetchQuantity and fetchValuation in an
+	// array("quantity" => value, "valuation" => value)
+	}
+}
