e006f8d42679f156f6bc0b6a62885d464e041f6b
Theodore R. Smith <theodore@phpexperts.pro>
2011-04-07 22:07:45 -0500

    Organize files in a more solid manner.

--- market/commodities/CommodityBasket.class.php
+++ market/commodities/CommodityBasket.class.php
@@ -7,31 +7,6 @@
 The realization I made was that we really have commodity stores
 and not just commodities. A store is defined as multiple of the same
 commodity.*/
-class CommodityStore
-{
-	/** @var Commodity **/
-	public $commodity;
-	public $quantity = 0;
-
-	public function __construct(Commodity $commodity = null, $quantity = 1)
-	{
-		if ($commodity !== null)
-		{
-			$this->commodity = $commodity;
-			$this->quantity = $quantity;
-		}
-	}
-
-	public function calculateWorth()
-	{
-		if ($this->commodity === null) { return 0.00; }
-
-		// The (float) bit is called Type casting. There's an article on it
-		// in PHPU if interested.
-		return (float)$this->commodity->currentValuation * (float)$quantity;
-	}
-}
-
 
 class CommodityBasket
 {
