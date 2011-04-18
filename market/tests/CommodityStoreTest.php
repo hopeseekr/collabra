062cae0340a8f184e4aa0eb9e686607110d07e93
Theodore R. Smith <theodore@phpexperts.pro>
2011-04-18 15:53:28 -0500

    Restore CommodityStoreTest.

--- market/tests/CommodityStoreTest.php
+++ market/tests/CommodityStoreTest.php
@@ -1,11 +1,19 @@
 <?php
-/** The Collabra Market Project
-  *   Part of the Collabra Commercial Collaboration Platform
-  *
-  * Copyright(c) 2011 Theodore R. Smith <theodore@phpexperts.pro>
-  * Copyright(c) 2011 Monica A. Chase <monica@phpexperts.pro>
-  * All rights reserved.
- **/
+/**
+* User Directory
+*   Copyright © 2008 Theodore R. Smith <theodore@phpexperts.pro>
+* 
+* The following code is licensed under a modified BSD License.
+* All of the terms and conditions of the BSD License apply with one
+* exception:
+*
+* 1. Every one who has not been a registered student of the "PHPExperts
+*    From Beginner To Pro" course (http://www.phpexperts.pro/) is forbidden
+*    from modifing this code or using in an another project, either as a
+*    deritvative work or stand-alone.
+*
+* BSD License: http://www.opensource.org/licenses/bsd-license.php
+**/
 
 require_once 'PHPUnit/Framework/TestCase.php';
 
@@ -18,6 +26,12 @@ class CommodityStoreTest extends PHPUnit_Framework_TestCase
 	 */
 	protected function setUp()
 	{
+		$commodity = new Commodity();
+		$commodity->name = "Test";
+		$commodity->currentValuation = 5;
+		$commodity->averageValuation = 3.2;
+
+		$this->commodityStore = new CommodityStore($commodity, 5);
 		parent::setUp();
 	}
 
@@ -34,6 +48,22 @@ class CommodityStoreTest extends PHPUnit_Framework_TestCase
 	 */
 	public function __construct()
 	{
+	}
+
+	public function testCanStoreACommodity()
+	{
+		$this->assertType('Commodity', $this->commodityStore->commodity);
+	}
+
+	public function testCanStoreMultipleQuantities()
+	{
+		$this->assertEquals(5, $this->commodityStore->quantity);
+	}
+
+	public function testCaculatesGrossValuation()
+	{
+		// 5 quantity * 5 value == 25
+		$this->assertEquals(25, $this->commodityStore->calculateWorth());
 	}
 }
 
