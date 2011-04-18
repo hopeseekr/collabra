a9aa249660f77373ffbd71e1e377cc7eb7342c52
Monica Chase <chase.mona@gmail.com>
2011-04-18 13:37:12 -0700

    Unit tests for classes

---
+++ market/tests/LoanManagerTest.php
@@ -0,0 +1,57 @@
+<?php
+/** The Collabra Market Project
+  *   Part of the Collabra Commercial Collaboration Platform
+  *
+  * Copyright(c) 2011 Theodore R. Smith <theodore@phpexperts.pro>
+  * Copyright(c) 2011 Monica A. Chase <monica@phpexperts.pro>
+  * All rights reserved.
+ **/
+
+require_once 'PHPUnit/Framework/TestCase.php';
+
+class LoanManagerTest extends PHPUnit_Framework_TestCase
+{
+	/** @var CommodityStore **/
+	private $commodityStore;
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp()
+	{
+		parent::setUp();
+	}
+
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown()
+	{
+		parent::tearDown();
+	}
+	
+	/**
+	 * Constructs the test case.
+	 */
+	public function __construct()
+	{
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
