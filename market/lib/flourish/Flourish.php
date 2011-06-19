f5bca10c2025b50d2a8ed7572857caaeb6d02423
Theodore R. Smith <theodore@phpexperts.pro>
2011-06-19 00:09:45 -0500

    Flourish init.

---
+++ market/lib/flourish/Flourish.php
@@ -0,0 +1,18 @@
+<?php
+
+class Flourish
+{
+	private function __construct() { }
+
+	protected static function autoload($className)
+	{
+		require FLOURISH_LIB_PATH . '/' . $className . '.php';
+	}
+
+	public static function init()
+	{
+		define('FLOURISH_LIB_PATH', dirname(__FILE__));
+		spl_autoload_register(array(__CLASS__, 'autoload'));
+	}
+}
+
