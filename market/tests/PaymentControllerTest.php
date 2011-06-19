fe0e67ee1fa0e1ff9186918bd51836ee5ac1622e
Theodore R. Smith <theodore@phpexperts.pro>
2011-06-19 14:37:00 -0500

    Moved out helper functions.

--- market/tests/PaymentControllerTest.php
+++ market/tests/PaymentControllerTest.php
@@ -51,30 +51,6 @@ class PaymentControllerTest extends PHPUnit_Extensions_OutputTestCase
 
 	}
 
-	protected function grabPageHTML($page)
-	{
-		$filename = CMARKET_LIB_PATH . '/views/' . $page  . '.tpl.php';
-		if (!file_exists($filename))
-		{
-			$this->assert(false, "Template file $page.tpl.php not found.");
-		}
-
-		ob_start();
-		include $filename;
-		$html = ob_get_clean();
-
-		return $html;
-	}
-
-	protected function buildPaymentBasket($commodityName, $quantity)
-	{
-		$commodity = CommoditiesFactory::build($commodityName);
-		$basket = new CommoditiesBasket;
-		$basket->add($commodity, $quantity);
-
-		return $basket;
-	}
-
 	public function testWillShowHomePageByDefault()
 	{
 		$homePageHTML = $this->grabPageHTML('home');
--- market/tests/bootstrap.inc.php
+++ market/tests/bootstrap.inc.php
@@ -2,3 +2,28 @@
 
 require_once realpath(dirname(__FILE__) . '/../Market.php');
 Market::init();
+
+function grabPageHTML($page)
+{
+    $filename = CMARKET_LIB_PATH . '/views/' . $page  . '.tpl.php';
+    if (!file_exists($filename))
+    {
+        $this->assert(false, "Template file $page.tpl.php not found.");
+    }
+
+    ob_start();
+    include $filename;
+    $html = ob_get_clean();
+
+    return $html;
+}
+
+function buildPaymentBasket($commodityName, $quantity)
+{
+    $commodity = CommoditiesFactory::build($commodityName);
+    $basket = new CommoditiesBasket;
+    $basket->add($commodity, $quantity);
+
+    return $basket;
+}
+
